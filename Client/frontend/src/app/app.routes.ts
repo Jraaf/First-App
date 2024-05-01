3e94ac50f3db06ed187c253a446ab470e1bb1046
Nasibov Rafael <nasibovrafael1@gmail.com>
2024-05-01 20:24:49 +0300

    change project architecture

---
+++ Client/frontend/src/app/app.routes.ts
@@ -0,0 +1,7 @@
+import { Routes } from '@angular/router';
+import { CardViewComponent } from './pages/card-view/card-view.component';
+
+export const routes: Routes = [
+    {path:'',
+    component:CardViewComponent}
+];
