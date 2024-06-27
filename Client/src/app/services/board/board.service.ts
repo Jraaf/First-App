7b52a11c2b15cd812e187fbe5994a15659879950
Nasibov Rafael <nasibovrafael1@gmail.com>
2024-06-27 14:16:49 +0300

    get requests | all entities

--- Client/src/app/services/board/board.service.ts
+++ Client/src/app/services/board/board.service.ts
@@ -2,7 +2,6 @@ import { Injectable } from '@angular/core';
 import {ApiService} from "../api.service";
 import {Observable} from "rxjs";
 import {Board} from "../../interfaces/Board";
-import {PaginationParams} from "../../interfaces/RequestOptions";
 
 @Injectable({
   providedIn: 'root'
